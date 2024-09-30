31dae03af9b7921f488788c45086828d80b2d6de
delacruzsamb@gmail.com <121100632+samueldelacruz@users.noreply.github.com>
2024-09-30 12:43:42 +0800

    Abstraction Learning Activity (revised)

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -11,4 +11,5 @@ public class Job
     {
         Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
     }
+
 }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,23 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Apple";
-        job1._startYear = 2016;
-        job1._endYear = 2018;
+        job1._jobTitle = "Professional Beggar";
+        job1._company = "Badjao Incorporated";
+        job1._startYear = 2005;
+        job1._endYear = 2050;
 
         Job job2 = new Job();
-        job2._jobTitle = "Manager";
-        job2._company = "Apple";
-        job2._startYear = 2018;
-        job2._endYear = 2024;
+        job2._jobTitle = "Professional Complainer";
+        job2._company = "Tambay Meet-up Company";
+        job2._startYear = 1999;
+        job2._endYear = 2099;
 
         Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
+        myResume._name = "Mc Lovin";
 
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
 
         myResume.Display();
+
     }
 }
