8e14f7fd52fd2f2baeef17a086e687fba23c2979
delacruzsamb@gmail.com <121100632+samueldelacruz@users.noreply.github.com>
2024-10-02 16:30:35 +0800

    Encapsulation Learning Activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,18 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(6, 7);
+
+        Console.WriteLine($"Fraction 1: {fraction1.GetFractionString()}");
+        Console.WriteLine($"Decimal Value 1: {fraction1.GetDecimalValue()}");
+
+        Console.WriteLine($"Fraction 2: {fraction2.GetFractionString()}");
+        Console.WriteLine($"Decimal Value 2: {fraction2.GetDecimalValue()}");
+
+        Console.WriteLine($"Fraction 3: {fraction3.GetFractionString()}");
+        Console.WriteLine($"Decimal Value 3: {fraction3.GetDecimalValue()}");
     }
 }
