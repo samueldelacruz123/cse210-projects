0141d9997e7f940249d0909936924d93c8c2a6d1
delacruzsamb@gmail.com <121100632+samueldelacruz@users.noreply.github.com>
2024-10-15 15:56:25 +0800

    Inheritance Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment assignment1 = new Assignment("Samuel Dela Cruz", "Programming with Classes");
+        string summary = assignment1.GetSummary();
+        Console.WriteLine(summary);
+
+        MathAssignment mathAssignment1 = new MathAssignment("Samuel Dela Cruz", "Exponents", "Section 7.3", "Problems 8-19");
+        string summary1 = mathAssignment1.GetSummary();
+        string homeworkList = mathAssignment1.GetHomeworkList();
+        Console.WriteLine(summary1);
+        Console.WriteLine(homeworkList);
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Samuel Dela Cruz", "European History", "The Causes of World War II");
+        string summary2 = writingAssignment1.GetSummary();
+        string writingInformation = writingAssignment1.GetWritingInformation();
+        Console.WriteLine(summary2);
+        Console.WriteLine(writingInformation);
     }
 }
