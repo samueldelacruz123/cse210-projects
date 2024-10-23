6170c07ba194241028f525cea2cbccffb6accdf2
delacruzsamb@gmail.com <121100632+samueldelacruz@users.noreply.github.com>
2024-10-23 15:50:45 +0800

    Polymorphism Learning Activity

---
+++ prepare/Learning06/Circle.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string color, double radius) : base()
+    {
+        SetColor(color);
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return _radius * _radius * Math.PI;
+    }
+}
--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,32 @@
 using System;
 
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        // Square square1 = new Square("Green", 4.0);  
+        // string color1 = square1.GetColor();
+        // double area1 = square1.GetArea();
+
+        // Rectangle rectangle2 = new Rectangle("Red", 8.0, 10.0);
+        // string color2 = rectangle2.GetColor();
+        // double area2 = rectangle2.GetArea();
+
+        // Circle circle3 = new Circle("Yellow", 5.0);
+        // string color3 = circle3.GetColor();
+        // double area3 = circle3.GetArea();
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("Green", 4.0));
+        shapes.Add(new Rectangle("Red", 8.0, 10.0));
+        shapes.Add(new Circle("Yellow", 5.0));
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"{color} {area}");
+        }
     }
 }
